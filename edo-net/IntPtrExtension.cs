c4bd71534636564106601799ba45acf839ab0e49
miniwa <blarch3030@gmail.com>
2017-08-06 15:12:07 +0200

    Added IntPtr extension file

---
+++ edo-net/IntPtrExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Edo
+{
+    /// <summary>
+    /// Extends IntPtr class with additional methods
+    /// </summary>
+    public static class IntPtrExtension
+    {
+        /// <summary>
+        /// Returns whether this IntPtr is a null pointer
+        /// </summary>
+        /// <param name="ptr">The pointer to be checked</param>
+        /// <returns></returns>
+        public static Boolean IsNullPtr(this IntPtr ptr)
+        {
+            return ptr == IntPtr.Zero;
+        }
+    }
+}
