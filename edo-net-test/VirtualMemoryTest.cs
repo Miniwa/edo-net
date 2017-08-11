5f547a759a9673427ab1b8cfbf323552e1eec46a
miniwa <blarch3030@gmail.com>
2017-08-11 16:26:08 +0200

    Changed variable signatures

--- edo-net-test/VirtualMemoryTest.cs
+++ edo-net-test/VirtualMemoryTest.cs
@@ -11,8 +11,8 @@ namespace Edo
     {
         public VirtualMemoryTest()
         {
-            Marshal = null;
-            ClosedMarshal = null;
+            Memory = null;
+            ClosedMemory = null;
             OutStream = new MemoryStream(32);
             Reader = new BinaryReader(OutStream);
         }
@@ -20,9 +20,9 @@ namespace Edo
         [TestInitialize]
         public void Init()
         {
-            Marshal = new VirtualMemory();
-            Marshal.Open(Process.GetCurrentProcess());
-            ClosedMarshal = new VirtualMemory();
+            Memory = new VirtualMemory();
+            Memory.Open(Process.GetCurrentProcess());
+            ClosedMemory = new VirtualMemory();
             OutStream.Seek(0, SeekOrigin.Begin);
             OutStream.SetLength(0);
         }
@@ -30,39 +30,39 @@ namespace Edo
         [TestMethod]
         public void TestInitialization()
         {
-            Assert.IsFalse(ClosedMarshal.IsOpen);
+            Assert.IsFalse(ClosedMemory.IsOpen);
         }
 
         [TestMethod]
         public void TestOpen()
         {
-            ClosedMarshal.Open(Process.GetCurrentProcess());
-            Assert.IsTrue(ClosedMarshal.IsOpen);
-            Assert.IsNotNull(ClosedMarshal.ProcessHandle);
+            ClosedMemory.Open(Process.GetCurrentProcess());
+            Assert.IsTrue(ClosedMemory.IsOpen);
+            Assert.IsNotNull(ClosedMemory.ProcessHandle);
         }
 
         [TestMethod]
         [ExpectedException(typeof(InvalidOperationException))]
         public void TestOpenWhenAlreadyOpen()
         {
-            ClosedMarshal.Open(Process.GetCurrentProcess());
-            ClosedMarshal.Open(1);
+            ClosedMemory.Open(Process.GetCurrentProcess());
+            ClosedMemory.Open(1);
         }
 
         [TestMethod]
         [ExpectedException(typeof(Win32Exception))]
         public void TestOpenThrowsOnApiError()
         {
-            ClosedMarshal.Open(-1);
+            ClosedMemory.Open(-1);
         }
 
         [TestMethod]
         public void TestClose()
         {
-            ClosedMarshal.Open(Process.GetCurrentProcess());
-            ClosedMarshal.Close();
-            Assert.IsFalse(ClosedMarshal.IsOpen);
-            Assert.IsNull(ClosedMarshal.ProcessHandle);
+            ClosedMemory.Open(Process.GetCurrentProcess());
+            ClosedMemory.Close();
+            Assert.IsFalse(ClosedMemory.IsOpen);
+            Assert.IsNull(ClosedMemory.ProcessHandle);
         }
 
         [TestMethod]
@@ -72,7 +72,7 @@ namespace Edo
             IntPtr address = (IntPtr)(&number);
 
             OutStream.SetLength(4);
-            Marshal.Read(address, OutStream.GetBuffer(), 4);
+            Memory.Read(address, OutStream.GetBuffer(), 4);
             Assert.AreEqual(number, Reader.ReadInt32());
         }
 
@@ -82,7 +82,7 @@ namespace Edo
             int number = 30;
             IntPtr address = (IntPtr)(&number);
 
-            Marshal.Read(address, OutStream, 4);
+            Memory.Read(address, OutStream, 4);
             OutStream.Seek(0, SeekOrigin.Begin);
             Assert.AreEqual(number, Reader.ReadInt32());
         }
@@ -91,25 +91,25 @@ namespace Edo
         [ExpectedException(typeof(ArgumentException))]
         public void TestReadThrowsIfBufferNotLargeEnough()
         {
-            Marshal.Read(IntPtr.Zero, OutStream.GetBuffer(), 10000);
+            Memory.Read(IntPtr.Zero, OutStream.GetBuffer(), 10000);
         }
 
         [TestMethod]
         [ExpectedException(typeof(InvalidOperationException))]
         public void TestReadThrowsIfNotOpen()
         {
-            ClosedMarshal.Read(IntPtr.Zero, OutStream.GetBuffer(), 4);
+            ClosedMemory.Read(IntPtr.Zero, OutStream.GetBuffer(), 4);
         }
 
         [TestMethod]
         [ExpectedException(typeof(Win32Exception))]
         public void TestReadThrowsOnApiError()
         {
-            Marshal.Read(IntPtr.Zero, OutStream.GetBuffer(), 4);
+            Memory.Read(IntPtr.Zero, OutStream.GetBuffer(), 4);
         }
 
-        public VirtualMemory Marshal { get; set; }
-        public VirtualMemory ClosedMarshal { get; set; }
+        public VirtualMemory Memory { get; set; }
+        public VirtualMemory ClosedMemory { get; set; }
         public MemoryStream OutStream { get; set; }
         public BinaryReader Reader { get; set; }
     }
